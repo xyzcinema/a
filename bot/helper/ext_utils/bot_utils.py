0c11cfd70dfcbaef40c65667b7bdca34d645e2bc
Bhaavana <158477850+Bhaavana-Official@users.noreply.github.com>
2024-02-02 11:02:25 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -141,11 +141,11 @@ def get_progress_bar_string(pct):
     p = min(max(pct, 0), 100)
     cFull = int(p // 8)
     cPart = int(p % 8 - 1)
-    p_str = '■' * cFull
+    p_str = '✦' * cFull
     if cPart >= 0:
-        p_str += ['▤', '▥', '▦', '▧', '▨', '▩', '■'][cPart]
-    p_str += '□' * (12 - cFull)
-    return f"[{p_str}]"
+        p_str += ['✧', '✧', '✧', '✧', '✧', '✧', '✧'][cPart]
+    p_str += '✧' * (12 - cFull)
+    return f"【{p_str}】"
 
 
 def get_all_versions():
