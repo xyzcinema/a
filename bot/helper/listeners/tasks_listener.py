bb55c1fea0e3317daf58513a616e36c9ad83feb8
Madflix Official <143621473+jishusinha@users.noreply.github.com>
2024-03-24 15:55:45 +0530

    Update tasks_listener.py

--- bot/helper/listeners/tasks_listener.py
+++ bot/helper/listeners/tasks_listener.py
@@ -461,14 +461,14 @@ class MirrorLeechListener:
                             
                         if config_dict['SAFE_MODE']:
                             if self.isSuperGroup:
-                                await sendMessage(self.botpmmsg, msg + BotTheme('L_LL_MSG') , btns, photo=self.random_pic)
+                                await sendMessage(self.botpmmsg, msg + BotTheme('L_LL_MSG') + fmsg, btns, photo=self.random_pic)
                             else:
                                 await sendMessage(self.message, message + fmsg, buttons.build_menu(2), photo=self.random_pic)
                         else:
                             if config_dict['SAVE_MSG'] and not saved and self.isSuperGroup:
                                 saved = True
                                 buttons.ibutton(BotTheme('SAVE_MSG'), 'save', 'footer')
-                            await sendMessage(self.message, message + fmsg, buttons.build_menu(2), photo=self.random_pic)
+                            await sendMessage(self.message, message , buttons.build_menu(2), photo=self.random_pic)
                         await sleep(1.5)
                         fmsg = ''
 
