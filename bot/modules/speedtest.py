cafae1fb1e4c8916fa234c34a1c7e781f2ceb340
Madflix Official <143621473+jishusinha@users.noreply.github.com>
2024-03-24 12:56:02 +0530

    Update speedtest.py

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -25,28 +25,28 @@ async def speedtest(_, message):
     path = result['share']
     string_speed = f'''
 ➲ <b><i>SPEEDTEST INFO</i></b>
-┠ <b>Upload:</b> <code>{get_readable_file_size(result['upload'] / 8)}/s</code>
-┠ <b>Download:</b>  <code>{get_readable_file_size(result['download'] / 8)}/s</code>
-┠ <b>Ping:</b> <code>{result['ping']} ms</code>
-┠ <b>Time:</b> <code>{result['timestamp']}</code>
-┠ <b>Data Sent:</b> <code>{get_readable_file_size(int(result['bytes_sent']))}</code>
-┖ <b>Data Received:</b> <code>{get_readable_file_size(int(result['bytes_received']))}</code>
+<b>Upload :</b> <code>{get_readable_file_size(result['upload'] / 8)}/s</code>
+<b>Download :</b>  <code>{get_readable_file_size(result['download'] / 8)}/s</code>
+<b>Ping :</b> <code>{result['ping']} ms</code>
+<b>Time :</b> <code>{result['timestamp']}</code>
+<b>Data Sent :</b> <code>{get_readable_file_size(int(result['bytes_sent']))}</code>
+<b>Data Received :</b> <code>{get_readable_file_size(int(result['bytes_received']))}</code>
 
 ➲ <b><i>SPEEDTEST SERVER</i></b>
-┠ <b>Name:</b> <code>{result['server']['name']}</code>
-┠ <b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
-┠ <b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
-┠ <b>Latency:</b> <code>{result['server']['latency']}</code>
-┠ <b>Latitude:</b> <code>{result['server']['lat']}</code>
-┖ <b>Longitude:</b> <code>{result['server']['lon']}</code>
+<b>Name :</b> <code>{result['server']['name']}</code>
+<b>Country :</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
+<b>Sponsor :</b> <code>{result['server']['sponsor']}</code>
+<b>Latency :</b> <code>{result['server']['latency']}</code>
+<b>Latitude :</b> <code>{result['server']['lat']}</code>
+<b>Longitude :</b> <code>{result['server']['lon']}</code>
 
 ➲ <b><i>CLIENT DETAILS</i></b>
-┠ <b>IP Address:</b> <code>{result['client']['ip']}</code>
-┠ <b>Latitude:</b> <code>{result['client']['lat']}</code>
-┠ <b>Longitude:</b> <code>{result['client']['lon']}</code>
-┠ <b>Country:</b> <code>{result['client']['country']}</code>
-┠ <b>ISP:</b> <code>{result['client']['isp']}</code>
-┖ <b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
+<b>IP Address :</b> <code>{result['client']['ip']}</code>
+<b>Latitude :</b> <code>{result['client']['lat']}</code>
+<b>Longitude :</b> <code>{result['client']['lon']}</code>
+<b>Country :</b> <code>{result['client']['country']}</code>
+<b>ISP :</b> <code>{result['client']['isp']}</code>
+<b>ISP Rating :</b> <code>{result['client']['isprating']}</code>
 '''
     try:
         pho = await sendMessage(message, string_speed, photo=path)
