d6c9076e4a5837c469c37e42f09811c2fa18c144
Madflix Official <143621473+jishusinha@users.noreply.github.com>
2024-03-30 21:20:24 +0530

    Update tasks_listener.py

--- bot/helper/listeners/tasks_listener.py
+++ bot/helper/listeners/tasks_listener.py
@@ -457,7 +457,7 @@ class MirrorLeechListener:
                 fmsg = '\n'
                 for index, (link, name) in enumerate(files.items(), start=1):
                     fmsg += f"{index}. <a href='{link}'>{name}</a>\n"
-                    if len(msg.encode() + fmsg.encode()) > 3900:
+                    if len(msg.encode() + fmsg.encode()) > (4000 if len(config_dict['IMAGES']) == 0 else 1000):
                             
                         if config_dict['SAFE_MODE']:
                             if self.isSuperGroup:
