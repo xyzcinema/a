0e95cdaa26e1ea903b85639669900f825eda81c8
brishabchy1991 <brishabchy1991@gmail.com>
2024-08-20 10:12:53 +0545

    Update update.py

--- update.py
+++ update.py
@@ -68,7 +68,7 @@ if DATABASE_URL is not None:
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = "https://github.com/Bhaavana-Official/WZML-X"
+    UPSTREAM_REPO = "https://github.com/brishabchy1991/a"
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
